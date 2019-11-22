c5ea292b3d05ebe791cabe6e4f61283f708af8a3
Chandler May <chandler.j.may@gmail.com>
2019-11-22 15:59:34 -0500

    Make paths environment-configurable.

--- docqa/config.py
+++ docqa/config.py
@@ -1,19 +1,22 @@
+from os import environ
 from os.path import join, expanduser, dirname
 
 """
 Global config options
 """
 
-VEC_DIR = join(expanduser("~"), "data", "glove")
-SQUAD_SOURCE_DIR = join(expanduser("~"), "data", "squad")
+DATA_DIR = environ.get("DATA_DIR", join(expanduser("~"), "data"))
+
+VEC_DIR = join(DATA_DIR, "glove")
+SQUAD_SOURCE_DIR = join(DATA_DIR, "squad")
 SQUAD_TRAIN = join(SQUAD_SOURCE_DIR, "train-v1.1.json")
 SQUAD_DEV = join(SQUAD_SOURCE_DIR, "dev-v1.1.json")
 
 
-TRIVIA_QA = join(expanduser("~"), "data", "triviaqa")
-TRIVIA_QA_UNFILTERED = join(expanduser("~"), "data", "triviaqa-unfiltered")
-LM_DIR = join(expanduser("~"), "data", "lm")
-DOCUMENT_READER_DB = join(expanduser("~"), "data", "doc-rd", "docs.db")
+TRIVIA_QA = join(DATA_DIR, "triviaqa")
+TRIVIA_QA_UNFILTERED = join(DATA_DIR, "triviaqa-unfiltered")
+LM_DIR = join(DATA_DIR, "lm")
+DOCUMENT_READER_DB = join(DATA_DIR, "doc-rd", "docs.db")
 
 
-CORPUS_DIR = join(dirname(dirname(__file__)), "data")
+CORPUS_DIR = environ.get("CORPUS_DIR", join(dirname(dirname(__file__)), "data"))
